dd0c86ec639a420e957c6974b486aa50e2ce1f46
Casey Opdahl <dpomondo@gmail.com>
2025-02-07 19:35:30 +0000

    added flask_socketio and some cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # iot_webserver
 
 A basic webserver made with python & Flask, to accept data sent from various Raspberry Pi Picos and ESp8266 boards, for the purpose of saving and displaying that information via a <gasp> web page.
+
+flask run --host=0.0.0.0 --debug
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 import time
 import csv
 import random
@@ -11,14 +12,18 @@ from bokeh.embed import file_html
 from utilities import make_filename
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 count = 0
 esp8266_data = 0
+data_dir = "data"
 
-if not os.path.isfile("data.pickle"):
-    fp = open("data.pickle", "x")
+
+if not os.path.isfile(data_dir + "/" + "data.pickle"):
+    # fp = open("data.pickle", "x")
+    fp = open(data_dir + "/" + "data.pickle", "x")
     fp.close()
 try:
-    with open("data.pickle", "rb") as fp:
+    with open(data_dir + "/" + "data.pickle", "rb") as fp:
         big_count = pickle.load(fp)
 except EOFError:
     # first time through should be 0
@@ -27,7 +32,7 @@ except EOFError:
 big_count += 1
 
 # update our launch count
-with open("data.pickle", "wb") as fp:
+with open(data_dir + "/" + "data.pickle", "wb") as fp:
     pickle.dump(big_count, fp)
 
 
@@ -45,9 +50,10 @@ def plot_data():
 
 @app.route('/query-data', methods=['GET'])
 def take_in_data():
-    global esp8266_data
-    fil_nam = make_filename()
-    esp8266_data = request.args.get('data')
+    # global esp8266_data
+    fil_nam = data_dir + "/" + make_filename()
+    # esp8266_data = request.args.get('data')
+    incoming_data = request.args.get('data')
 
     tim_stam = time.asctime()
     results = []
@@ -61,10 +67,14 @@ def take_in_data():
         writer.writerow(results)
 
     # return f"data: {request.args.get('data')}"
-    return request.args.get('data')
+    # socketio.emit('esp_data', {'data': incoming_data})
+    socketio.emit('esp_data', {'data': incoming_data}, namespace='/')
+    # return request.args.get('data')
+    return incoming_data
 
 
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     global big_count
     global count
@@ -73,9 +83,9 @@ def index():
     #     'title': "Hello!",
     #     'num': count
     # }
-    if request.method == 'POST':
-        temp_num = 99
-    elif request.method == 'GET':
+    # if request.method == 'POST':
+    #     temp_num = 99
+    if request.method == 'GET':
         temp_num = count
     return render_template('index.html',
                            title="Hello!",
@@ -85,5 +95,28 @@ def index():
                            )
 
 
+@socketio.on('connect')
+def handle_connect():
+    print(f"Client {request.sid} connected")
+    emit("data", esp8266_data)
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print(f"Client {request.sid} disconnected")
+
+
+@socketio.on('esp_data')
+def handle_data(json):
+    print(f"data event, received: {str(json)}")
+    emit("data", esp8266_data)
+
+
+@socketio.on('my event')
+def handle_my_event(json):
+    print(f"received: {str(json)}")
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    # app.run(debug=True, port=80, host='0.0.0.0')
+    socketio.run(app, host="0.0.0.0", debug=True)
